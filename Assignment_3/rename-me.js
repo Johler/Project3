b8bbb256ce9dc0e73cc3f633eaaa1e88229d325a
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-24 10:17:01 -0400

    Added JSON data, function, for loop, nested loop

--- Assignment_3/rename-me.js
+++ Assignment_3/rename-me.js
@@ -1,1 +1,50 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+// Jessica Ohler
+// SDI 1407
+// Project 3
+
+//JSON Data Shows the family that is going to the amusement park, their age, and their favorite rides. 
+var jsonData = {
+	"familyMembers":
+		[
+			{
+			"name" : "Albert",
+			"age" : 50,
+			"favoriteRide" : "The Raptor"
+			},
+			{
+			"name" : "Claudia",
+			"age" : 49,
+			"favoriteRide" : "The Magnum"
+			},
+			{
+			"name" : "Josh",
+			"age" : 30,
+			"favoriteRide" : "The Mantis"
+			},
+			{
+			"name" : "Joie",
+			"age" : 26,
+			"favoriteRide" : "The Millennium Force"
+			},
+			{
+			"name" : "Sam",
+			"age" : 18,
+			"favoriteRide" : "The Corkscrew"
+			},
+			{
+			"name" : "Riley",
+			"age" : 17,
+			"favoriteRide" : "Top Thrill Dragster"
+			}
+		]
+};
+
+var myJSON = function() {
+	for (var key in jsonData.familyMembers){
+		for (var newKey in jsonData.familyMembers[key]) {
+			console.log(key + ":" + newKey + ":" + jsonData.familyMembers[key][newKey]);
+		}
+	};
+};
+myJSON();
