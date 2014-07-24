2476f3e24047eb0c099610250d63595b71b40e3d
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-24 12:16:57 -0400

    Added Number function with math and conditionals

--- Assignment_3/rename-me.js
+++ Assignment_3/rename-me.js
@@ -3,6 +3,17 @@
 // SDI 1407
 // Project 3
 
+//Global Variables
+var distanceFromParkMiles = 60,
+	rideNames = ["The Raptor",
+				"The Magnum",
+				"The Mantis",
+				"The Millennium Force",
+				"The Corkscrew",
+				"The Top Thrill Dragster"],
+	minutesInLine = [25, 45, 30, 65, 20, 125]
+;
+
 //JSON Data Shows the family that is going to the amusement park, their age, and their favorite rides. 
 var jsonData = {
 	"familyMembers":
@@ -35,7 +46,7 @@ var jsonData = {
 			{
 			"name" : "Riley",
 			"age" : 17,
-			"favoriteRide" : "Top Thrill Dragster"
+			"favoriteRide" : "The Top Thrill Dragster"
 			}
 		]
 };
@@ -48,3 +59,39 @@ var myJSON = function() {
 	};
 };
 myJSON();
+
+//Driving to Cedar Point from home with the family. Using a while loop. Getting gas for the trip using Number Function.
+	
+while (distanceFromParkMiles > 0) {
+		console.log("We are still " + distanceFromParkMiles + "miles from the park!");
+		distanceFromParkMiles -=5; 
+};
+	
+console.log ("We made it to the park. Yipee!");
+
+//Gas used on the way there.
+
+var getGas = function (cash){
+	var pricePerGallon = 3,
+	gasTankMax = 14,
+	gasTankHoldsGallons;
+	
+	if (cash < pricePerGallon) {
+		console.log("We need more money for gas!")
+			if (gasTankHolds > gasTankMax) {
+				console.log("Gas tank is full. Let's hit the road!");
+			}
+		} else {
+		gasTankHoldsGallons = (cash / pricePerGallon);
+		}
+		return gasTankHoldsGallons;
+};
+var gasFilled = getGas(25)
+console.log("We got " + gasFilled + " gallons in our tank.");
+
+//Number Function with For Loop  Waitin
+var waitingInLine = function (ride) {
+	var rideNames = [ride],
+		minutesThisLine = minutesInLine[rideNumber];
+	console.log("At " + rideName + " we waited in line for " + minutesThisLine + " minutes.");
+};
